d7ca7ff28f97829a947b7709a33b9e93eb1a3f3a
PedroCosta2023 <lucaspedrolucio@gmail.com>
2024-06-08 15:12:37 -0300

    update app

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
-import CreativeComponent from "./src/class-component/native-component";
+import { View } from "react-native";
+import Card from "./src/type-weather/pages/dash/components/card";
+import Search from "./src/type-weather/pages/search";
+import WeatherDetail from "./src/type-weather/pages/dash/components/weather-detail";
 
 export default function App() {
-  return <CreativeComponent />;
+
+  return (
+    <View style={{display:"flex",justifyContent:"center",alignItems:"center", height: 500, backgroundColor:"Black"}}>
+      <WeatherDetail/>
+    </View> 
+    
+  )
 }
